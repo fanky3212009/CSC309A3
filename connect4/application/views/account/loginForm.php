d5bc221a641245d4b50f6788d563cdcfb3d6116a
MShwed <matthew.shwed@gmail.com>
2013-11-24 12:24:23 -0500

    Updated with bootstrap

--- connect4/application/views/account/loginForm.php
+++ connect4/application/views/account/loginForm.php
@@ -1,40 +1,65 @@
 
 <!DOCTYPE html>
-
 <html>
 	<head>
-		<style>
-			input {
-				display: block;
-			}
-		</style>
-
+		<?php $this->load->view('header');?>
 	</head> 
 <body>  
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <!--<form class="navbar-form navbar-right">-->
+          <!--  <div class="form-group"> -->
+            	<?php 
+            		$attributes = array('class' => 'navbar-form navbar-right'
+            							);
+            		$attributes2 = array('class' => 'form-control',
+            							'required' => "TRUE",
+            							'name' => "username"
+            							);
+            		
+            		echo form_open('account/login', $attributes);
+            		echo '<div class="form-group">';
+            		echo form_label('Username');
+            		echo form_error('username');
+					//echo form_input('username',set_value('username'),"required", $attributes2);
+					echo form_input( $attributes2);
+            	
+              echo '<!--<input type="text" placeholder="Email" class="form-control">-->
+            </div>
+            <div class="form-group">';
+	            
+	        		echo form_label('Password'); 
+					echo form_error('password');
+					echo form_password('password','',"required");	
+	            ?>
+              <!--<input type="password" placeholder="Password" class="form-control">-->
+            </div>
+            	<?php 	
+            		echo form_submit('submit', 'Login');
+            		echo form_close();
+            	?>
+
+            <!--<button type="submit" class="btn btn-success">Sign in</button>-->
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
+    <div class="container">
 	<h1>Login</h1>
-<?php 
-	if (isset($errorMsg)) {
-		echo "<p>" . $errorMsg . "</p>";
-	}
+	</div>
 
-
-	echo form_open('account/login');
-	echo form_label('Username'); 
-	echo form_error('username');
-	echo form_input('username',set_value('username'),"required");
-	echo form_label('Password'); 
-	echo form_error('password');
-	echo form_password('password','',"required");
-	
-	echo form_submit('submit', 'Login');
-	
-	echo "<p>" . anchor('account/newForm','Create Account') . "</p>";
-
-	echo "<p>" . anchor('account/recoverPasswordForm','Recover Password') . "</p>";
-	
-	
-	echo form_close();
-?>	
+ 	
+   
 </body>
 
 </html>
---
+++ connect4/application/views/header.php
@@ -0,0 +1,17 @@
+<title>
+	Connect 4
+</title>
+
+<script type="text/javscript">
+	//<![CDATA[
+	base_url = '<?= base_url();?>';
+	//]]>
+</script>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script type="text/javascript" src="<?php echo base_url();?>libraries/jQuery/jquery-2.0.3.min.js"></script>
+
+<!-- Bootstrap -->
+<script type="text/javascript" src="<?php echo base_url();?>libraries/bootstrap/js/bootstrap.min.js"></script>
+<link href="<?php echo base_url();?>libraries/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url();?>css/template.css" rel="stylesheet" type="text/css" />
